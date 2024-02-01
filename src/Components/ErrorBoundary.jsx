a170b7dee0777ee2b5fb309e3b637e692f022a62
nandhini perumal_FSD_developer <150987579+nandhini1227@users.noreply.github.com>
2024-02-01 10:05:31 +0530

    Add files via upload

---
+++ src/Components/ErrorBoundary.jsx
@@ -0,0 +1,15 @@
+﻿class ErrorBoundary extends React.Component {
+  componentDidCatch(error, errorInfo) {
+    // Handle the error
+    console.error(error, errorInfo);
+  }
+
+  render() {
+    return this.props.children;
+  }
+}
+
+// Wrap your entire application with the ErrorBoundary
+<ErrorBoundary>
+  <App />
+</ErrorBoundary>
---
+++ src/Components/Header.jsx
@@ -0,0 +1,10 @@
+﻿
+import './Header.css';
+import blogHeader from '../assets/blogHeader.png'
+
+export default function Header() {
+
+    return (
+        <img className="pic" src={blogHeader} alt="header" />
+    )
+}
